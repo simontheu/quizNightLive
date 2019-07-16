66d2bdcc84b05f94da8d0286fc481da14d67624e
root <root@localhost.localdomain>
2019-07-16 22:37:05 +0100

    updated questions

--- index_script.js
+++ index_script.js
@@ -1,3 +1,47 @@
+//Get a cookie for this quiz entry
+function uuidv4() {
+  return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+  )
+}
+
+function getUniqueKey() {
+  var uniqueKey = getCookie("uniqueKey");
+  if (uniqueKey != "") {
+    return uniqueKey
+  } else {
+    newKey = uuidv4()
+    if (newKey != "" && newKey != null) {
+      setCookie("uniqueKey", newKey, 365);
+      return newKey
+    }
+  }
+}
+
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  var expires = "expires="+d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
 //Initialise connecton to socket system
 var socket = io();
 
@@ -9,134 +53,15 @@ timer.addEventListener('secondsUpdated', function (e) {
   updateVisibleTime();
 });
 
-socket.on('awayScore', function(msg){
-  scoreNum = Number(document.getElementById("awayScoreVal").textContent);
-  scoreNum = scoreNum + Number(msg);
-  if (scoreNum < 10) {
-    scoreText = ' ' + String(scoreNum);
-  } else {
-    scoreText = String(scoreNum);
-  }
-  document.getElementById("awayScoreVal").textContent =  scoreText;
-  document.getElementById("awayScoreLower3rdVal").textContent =  scoreText;
-
-  socket.emit('awayScoreUpdate', scoreText);
-
+socket.on('gotQuestion', function(msg){
+  document.getElementById("question").textContent = "Q1: Questin goes here?"
+  document.getElementById("answerA").textContent = "Answer A"
+  document.getElementById("answerB").textContent = "Answer B"
+  document.getElementById("answerC").textContent = "Answer C"
+  document.getElementById("answerD").textContent = "Answer D"
+  //socket.emit("lowerThirdScoreBackgroundAnnounce","/media/HalfTime.png")
 });
 
-socket.on('homeScore', function(msg){
-  scoreNum = Number(document.getElementById("homeScoreVal").textContent);
-  scoreNum = scoreNum + Number(msg);
-  if (scoreNum < 10) {
-    scoreText = ' ' + String(scoreNum);
-  } else {
-    scoreText = String(scoreNum);
-  }
-  document.getElementById("homeScoreVal").textContent =  scoreText;
-  document.getElementById("homeScoreLower3rdVal").textContent =  scoreText;
-
-  socket.emit('homeScoreUpdate', scoreText);
-
-});
-
-socket.on('clockAnim', function(msg){
-  if (Number(msg) == 1) {
-    document.getElementById("scoreClockDiv").className = "rotateIn";
-    socket.emit('clockOnAirAnnounce',1);
-  } else {
-    document.getElementById("scoreClockDiv").className = "rotateOut";
-    socket.emit('clockOnAirAnnounce',0);
-  }
-});
-
-socket.on('lowerThirdScore', function(msg){
-  if (Number(msg) == 1) {
-    document.getElementById("lowerThirdScoreDiv").className = "lowerThirdScoreIn";
-    socket.emit("lowerThirdScoreOnAirAnnounce", 1);
-  } else {
-    document.getElementById("lowerThirdScoreDiv").className = "lowerThirdScoreOut";
-    socket.emit("lowerThirdScoreOnAirAnnounce", 0);
-  }
-  
-});
-
-socket.on('setLowerThirdScoreBackground', function(msg){
-  console.log("test...");
-
-  if (Number(msg) == 1) {
-    document.getElementById("lowerThirdScoreBackground").src = "/media/half_time_lower_3rd.png";
-    socket.emit("lowerThirdScoreBackgroundAnnounce","/media/half_time_lower_3rd.png")
-  } else {
-    document.getElementById("lowerThirdScoreBackground").src = "/media/full_time_lower_3rd.png";
-    socket.emit("lowerThirdScoreBackgroundAnnounce","/media/full_time_lower_3rd.png")
-  }
-  
-});
-
-socket.on('clockTime', function(msg){
-  if (Number(msg) == 1) {
-    //Start Clock
-    timer.start();
-  } else if (Number(msg) == 0){
-    //Stop Clock
-    timer.pause();//This is a better stop, than the stop that resets also.
-  } else if (Number(msg) == 2){//Reset
-    //Stop Clock
-    timer.stop();
-    updateVisibleTime();
-  }
-  
-});
-
-socket.on('halfUpdate', function(msg){
-  //document.getElementById("clockScoreBackground").className = "rotateOut";
-  if (Number(msg) == 1) {
-    //Set 1st half
-    document.getElementById("halfIndicator").textContent = "1st";
-    socket.emit('halfAnnounce', "1st");
-  } else {
-    //Set 2nd half
-    document.getElementById("halfIndicator").textContent = "2nd";
-    socket.emit('halfAnnounce', "2nd");
-  } 
-});
-
-socket.on('timeAdjust', function(msg){
-  //document.getElementById("clockScoreBackground").className = "rotateOut";
-  //process the hours to minutes problem here
-  var minutesVal = Number(msg.substring(0,2));
-  var secondsVal = Number(msg.substring(3,5));//01:34:67
-
-  timer.stop();//Cant get it to work without this 
-  timer.start ( {startValues: { minutes: minutesVal, seconds: secondsVal }} );
-
-  
-  updateVisibleTime();
-});
-
-socket.on('homeL3', function(msg){
-  var playerNum = teams.home[msg].number;
-  var playerName = teams.home[msg].name;
-  var l3Text = playerNum + " " + playerName;
-  if (document.getElementById("lowerThirdHomePlayerDiv").className == "lowerThirdScoreIn") {
-    document.getElementById("lowerThirdHomePlayerDiv").className = "lowerThirdScoreOut";
-  } else {
-    document.getElementById("homeLower3rdNameVal").textContent = l3Text;
-    document.getElementById("lowerThirdHomePlayerDiv").className = "lowerThirdScoreIn";
-  }
-});
-
-socket.on('awayL3', function(msg){
-  var playerNum = teams.away[msg].number;
-  var playerName = teams.away[msg].name;
-  var l3Text = playerNum + " " + playerName;
-  if (document.getElementById("lowerThirdAwayPlayerDiv").className == "lowerThirdScoreIn") {
-    document.getElementById("lowerThirdAwayPlayerDiv").className = "lowerThirdScoreOut";
-  } else {
-    document.getElementById("awayLower3rdNameVal").textContent = l3Text;
-    document.getElementById("lowerThirdAwayPlayerDiv").className = "lowerThirdScoreIn";
-  }
-});
 
 function updateVisibleTime()
 {
@@ -155,7 +80,75 @@ function updateVisibleTime()
   socket.emit('timeAnnounce',timeString);
 }
 
-socket.on('gotTeams', function(msg){
-  console.log("got teams", msg);
+socket.on('gotNewQuestion', function(msg){
+  console.log("gotNewQuestion", msg);
   teams = msg;
 });
+
+
+function answer(answer) {
+  var uniqueKey = getUniqueKey()
+  stopTheClock()
+  submitAnswer(answer, elapsedTime, uniqueKey)
+  switch (answer) {
+    case "A":
+      var answerElement = document.getElementById("answerA");
+      answerElement.style.backgroundColor = "#ff0000";
+      break;
+    case "B":
+      var answerElement = document.getElementById("answerB");
+      answerElement.style.backgroundColor = "#ff0000";
+      break;
+    case "C":
+      var answerElement = document.getElementById("answerC");
+      answerElement.style.backgroundColor = "#ff0000";
+      break;
+    case "D":
+      var answerElement = document.getElementById("answerD");
+      answerElement.style.backgroundColor = "#ff0000";
+      break;
+  }
+}
+
+function animateTimer() {
+  startTime = Date.now();
+  timer = setInterval(frame, 20);
+}
+
+function frame() {
+  var questionLength = 30000;
+  var progess = document.getElementById("progress");
+  elapsedTime = questionLength - (Date.now() - startTime);
+  
+  updateTimerBar((elapsedTime/questionLength));
+
+  if (elapsedTime < 1) {
+    stopTheClock()
+  }
+}
+
+function stopTheClock() {
+  elapsedTime = 0;
+  clearInterval(timer);
+}
+
+function updateTimerBar(fractionLeft) {
+  var c = document.getElementById("timerBarCanvas");
+  var ctx = c.getContext("2d");
+  var fillWidth = c.width * fractionLeft;
+  ctx.fillRect(0, 0, fillWidth, c.height);
+  ctx.clearRect(fillWidth, 0, c.width, c.height);
+}
+
+function submitAnswer(answer, elapsedTime, uniqueKey) {
+  var answerObject = {
+    "answer":answer,
+    "elapsedTime":elapsedTime,
+    "userKey":uniqueKey,
+    "questionID":"1",
+  }
+  console.log(answer)
+  console.log(elapsedTime)
+  console.log(uniqueKey)
+  socket.emit('answeredQuestion',answerObject);
+}
